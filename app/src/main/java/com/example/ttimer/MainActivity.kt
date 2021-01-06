df86b7d9f988c8673566637ab67eae267e4a1564
alexander.schubert <alex.schubi2000@gmail.com>
2021-01-06 16:27:13 +0100

    LAYERing for main and add

--- app/src/main/java/com/example/ttimer/MainActivity.kt
+++ app/src/main/java/com/example/ttimer/MainActivity.kt
@@ -7,10 +7,14 @@ import kotlinx.android.synthetic.main.activity_add.*
 import kotlinx.android.synthetic.main.activity_main.*
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
+import android.content.Context
+import android.content.Context.INPUT_METHOD_SERVICE
 import android.content.Intent
+import android.inputmethodservice.Keyboard
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.*
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -18,8 +22,6 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity()
 {
-
-
     //VARIABLES
    // val intent = Intent(this, SecondActivity::class.java)
     var delmode: Boolean = false
@@ -29,17 +31,22 @@ class MainActivity : AppCompatActivity()
     private val exList = ArrayList<Item>()
     private val adapter = RVadapter(exList)
     private var index: Int = 0
+    var testText: String = ""
     //START
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        this.layer2.visibility = View.INVISIBLE
+        this.layer1.visibility = View.VISIBLE
 
 //-------------ADDING
         b_add.setOnClickListener() {
             Toast.makeText(this, "b_add clicked", Toast.LENGTH_SHORT).show()
+            this.layer1.visibility = View.INVISIBLE
+            this.layer2.visibility = View.VISIBLE
+
             //setContentView(R.layout.activity_add)
-            R.layout.activity_add.bringToFront()
         }
         //DELMODE
         b_del.setOnClickListener() {
@@ -59,33 +66,56 @@ class MainActivity : AppCompatActivity()
 
 
 
-//CREATE LISTE TODO
+//CREATE LISTE TODO recycler View
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.setHasFixedSize(true)
     }
 
-
     //-------------------ADDITEM
     //TODO fragment host erstellen
+    fun openAdd(view: View){
+        Toast.makeText(this, "Button-ADD clicked", Toast.LENGTH_SHORT)
+    }
+
     fun addItem(view: View) {
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
 
         addText = tb_add_text.text.toString()
         addDate = calendarView.date.toString()
         addTime = tb_add_time.text.toString()
-        index++
+        index += 1
 
-        val newItem = Item(addText, addDate, addTime)
+        /*val newItem = Item(addText, addDate, addTime)
         exList.add(index, newItem)
-        adapter.notifyItemInserted(index)
+        adapter.notifyItemInserted(index)*/
 
+        //TEST-output
+        var addindex: String =index.toString()
+
+        testText += "$addindex $addText $addDate $addTime \n"
+        tv_test_out.text = testText
+
+
+        hideKeyboard()
+        this.layer1.visibility = View.VISIBLE
+        this.layer2.visibility = View.INVISIBLE
         Toast.makeText(this, "b_add_final clicked", Toast.LENGTH_SHORT).show()
     }
     fun deleteItem (view: View){
 
     }
-//TEST
+
+    //HIDE KEYBOARD
+    fun MainActivity.hideKeyboard() {
+        hideKeyboard(currentFocus ?: View(this))
+    }
+    fun Context.hideKeyboard(view: View) {
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    //TEST
     private fun generateDummyList(size: Int): ArrayList<Item> {
         val list = ArrayList<Item>()
         for (i in 0 until size) {
