0a02b3bd0e71a589b719c375e9a206d6e4aae234
alexander.schubert <alex.schubi2000@gmail.com>
2021-04-08 16:00:29 +0200

    8.4.21 work separate Functions.kt added SwipetoDelete for RVadapter.kt as items-deleting rewrite code fixes known Errors: swipetoDelete ot clearly working

--- app/src/main/java/com/example/ttimer/RVadapter.kt
+++ app/src/main/java/com/example/ttimer/RVadapter.kt
@@ -1,22 +1,14 @@
 package com.example.ttimer
 
-import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.TextView
 import android.widget.Toast
-import androidx.annotation.MainThread
-import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.recycler_view.view.*
 import java.time.format.DateTimeFormatter
-import kotlin.coroutines.coroutineContext
 
 
 class RVadapter(private val rVArrayList: ArrayList<Item>) : RecyclerView.Adapter<RVadapter.ViewHolder>(){
@@ -40,14 +32,15 @@ class RVadapter(private val rVArrayList: ArrayList<Item>) : RecyclerView.Adapter
 
     override fun getItemCount() = rVArrayList.size
 
+    fun deleteItem(position: Int) {
+        rVArrayList.removeAt(position)
+        notifyItemRemoved(position)
+    }
+
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
         init { itemView.setOnClickListener(this) }
-        //private val delmode: Boolean = true
-        private var view: View = itemView
-        private var item: Item? = null
-
         override fun onClick(itemView: View) {
-            val prefIndex = itemView.id
+            /*val prefIndex = itemView.id
             if(delmode) {
                 //TODO select deleting and keep items
                 itemView.setBackgroundResource(R.color.button_select)
@@ -55,17 +48,28 @@ class RVadapter(private val rVArrayList: ArrayList<Item>) : RecyclerView.Adapter
                 Toast.makeText( itemView.context, "Item $prefIndex deleted", Toast.LENGTH_SHORT).show()
             }else{
                 Toast.makeText( itemView.context, "Item Pref $prefIndex clicked", Toast.LENGTH_SHORT).show()
-                //MainActivity().editItem(index)
-                /*MainActivity().layer2.visibility = View.VISIBLE
-                itemView.layer1.visibility = View.INVISIBLE
-                addmode = true
-                itemView.tb_add_text.setText(getArrayList[index].Text)
-
-                itemView.datePicker.updateDate(getArrayList[index].Date.year, getArrayList[index].Date.monthValue, getArrayList[index].Date.dayOfMonth)
-                itemView.timePicker.hour = getArrayList[index].Date.hour
-                itemView.timePicker.minute = getArrayList[index].Date.minute*/
-            }
+            }*/
         }
+    }
+}
+class SwipeToDelete(var adapter: RVadapter) : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT){
+    override fun onMove(
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        target: RecyclerView.ViewHolder
+    ): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+        var position = viewHolder.adapterPosition
+        val item = viewHolder.itemView.id
+        adapter.deleteItem(position)
+        mainPrefs.edit().remove("Item $item").apply()
+        Toast.makeText(viewHolder.itemView.context, "Item $item deleted", Toast.LENGTH_SHORT).show()
+        Log.d("RecyclerView.swiped","adapterpos $position = ID $item")
+        Log.d("SharedPreferences", "deleted Item $item")
     }
 
 }
+
