d4e29ccf2765d2271d2e895b02e5455e166479f2
alexschubi <alex.schubi2000@gmail.com>
2023-01-18 01:45:08 +0100

    18.1.23-home implementing chips

--- app/src/main/java/xyz/alexschubi/ttimer/tabs/NotesList.kt
+++ app/src/main/java/xyz/alexschubi/ttimer/tabs/NotesList.kt
@@ -3,63 +3,62 @@ package xyz.alexschubi.ttimer.tabs
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.Text
+import androidx.compose.material.Chip
+import androidx.compose.material.ChipDefaults
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import xyz.alexschubi.ttimer.data.Category
-import xyz.alexschubi.ttimer.data.Tag
-import xyz.alexschubi.ttimer.data.kNote
+import xyz.alexschubi.ttimer.data.*
 
+@OptIn(ExperimentalMaterialApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun ItemsScreen() {
     LazyColumn(
-        contentPadding = PaddingValues(4.dp),
+        contentPadding = PaddingValues(8.dp),
         verticalArrangement = Arrangement.spacedBy(4.dp)
     ) {
         itemsIndexed(testData()){ index, item ->
             Card(
                 modifier = Modifier.fillMaxWidth(),
-                colors = CardDefaults.cardColors(containerColor =  MaterialTheme.colorScheme.surfaceVariant)
+                colors = CardDefaults.cardColors(containerColor =  MaterialTheme.colorScheme.secondaryContainer)
             ){
-                Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
-                    Text(text = item.uid.toString())
-                    Column {
-                        Text(
-                            color = MaterialTheme.colorScheme.primary,
-                            text = item.category.toString()
-                        )
-                    }
-                    Column{
-                        Row() {
-                            item.tags?.forEach { tag ->
-                                Button(
-                                    shape = MaterialTheme.shapes.extraSmall,
-                                    modifier = Modifier
-                                        .height(20.dp)
-                                        .defaultMinSize(minWidth = 1.dp, minHeight = 1.dp),
-                                    contentPadding = PaddingValues(2.dp, 0.dp),
-                                    colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.secondary),
-                                    onClick = { /*TODO*/ }
-                                ) {
-                                    Text(
-                                        text = tag.toString(),
-                                        color = MaterialTheme.colorScheme.primary,
-                                        fontSize = 10.sp)
-                                }
-                            }
-                        }
-                    }
-                }
+                //row with Text
                 Row(modifier = Modifier.fillMaxWidth()) {
                     Text(text = item.text)
                 }
-
+                //row with Notifications
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(2.dp, 0.dp, 2.dp, 2.dp)) {
+                    //add TAGS
+                    item.notifications?.forEach { notification ->
+                        AssistChip(
+                            shape = MaterialTheme.shapes.extraSmall,
+                            modifier = Modifier.padding(2.dp).height(18.dp),
+                            label = { Text(text = notification.timestamp.toString(), fontSize = 12.sp)},
+                            onClick = { /*TODO*/ }
+                        )
+                    }
+                }
+                //row with Tags
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(2.dp, 0.dp, 2.dp, 2.dp)) {
+                    //add TAGS
+                    item.tags?.forEach { tag ->
+                        AssistChip(
+                            shape = MaterialTheme.shapes.extraSmall,
+                            modifier = Modifier.padding(2.dp).height(18.dp),
+                            label = { Text(text = tag.toString(), fontSize = 12.sp)},
+                            onClick = { /*TODO*/ }
+                        )
+                    }
+                    Text(text = item.uid.toString())
+                }
             }
-
         }
     }
 }
@@ -68,13 +67,18 @@ fun ItemsScreen() {
 //TEST
 fun testData(): List<kNote>{
     return listOf(
-        kNote(1, null, "test1", 1673894238666, Category.purple, listOf(Tag.alpha, Tag.beta), null),
-        kNote(2, null, "test2", 1673894238666, Category.purple, listOf(Tag.alpha, Tag.beta), null),
-        kNote(3, null, "test3", 1673894238666, Category.purple, listOf(Tag.alpha, Tag.beta), null),
-        kNote(4, null, "test4", 1673894238666, Category.purple, listOf(Tag.alpha, Tag.beta), null),
-        kNote(5, null, "test5", 1673894238666, Category.purple, listOf(Tag.alpha, Tag.beta), null),
-        kNote(6, null, "test6", 1673894238666, Category.purple, listOf(Tag.alpha, Tag.beta), null),
-        kNote(7, null, "test7", 1673894238666, Category.purple, listOf(Tag.alpha, Tag.beta), null),
-        kNote(8, null, "test8", 1673894238666, Category.purple, listOf(Tag.alpha, Tag.beta), null)
-    )
+        kNote(1, null, "test1", 1673894238666, Category.purple, listOf(Tag.alpha, Tag.beta), listOf( kNotification(1, NotificationStatus.new, 2673894238666), kNotification(2, NotificationStatus.pending, 2673894238666))),
+        kNote(2, null, "test2", 1673894238666, Category.purple, listOf(Tag.alpha, Tag.beta), listOf( kNotification(1, NotificationStatus.new, 2673894238666), kNotification(2, NotificationStatus.pending, 2673894238666))),
+        kNote(3, null, "test3", 1673894238666, Category.purple, listOf(Tag.alpha, Tag.beta), listOf( kNotification(1, NotificationStatus.new, 2673894238666), kNotification(2, NotificationStatus.pending, 2673894238666))),
+        kNote(4, null, "test4", 1673894238666, Category.purple, listOf(Tag.alpha, Tag.beta), listOf( kNotification(1, NotificationStatus.new, 2673894238666), kNotification(2, NotificationStatus.pending, 2673894238666))),
+        kNote(5, null, "test5", 1673894238666, Category.blue, listOf(Tag.alpha, Tag.beta), listOf( kNotification(1, NotificationStatus.new, 2673894238666), kNotification(2, NotificationStatus.pending, 2673894238666))),
+        kNote(6, null, "test6", 1673894238666, Category.blue, listOf(Tag.alpha, Tag.beta), listOf( kNotification(1, NotificationStatus.new, 2673894238666), kNotification(2, NotificationStatus.pending, 2673894238666))),
+        kNote(7, null, "test7", 1673894238666, Category.green, listOf(Tag.alpha, Tag.beta), listOf( kNotification(1, NotificationStatus.new, 2673894238666), kNotification(2, NotificationStatus.pending, 2673894238666))),
+        kNote(8, null, "test8", 1673894238666, Category.green, listOf(Tag.alpha, Tag.beta), listOf( kNotification(1, NotificationStatus.new, 2673894238666), kNotification(2, NotificationStatus.pending, 2673894238666))),
+        kNote(9, null, "test3", 1673894238666, Category.green, listOf(Tag.alpha, Tag.beta), listOf( kNotification(1, NotificationStatus.new, 2673894238666), kNotification(2, NotificationStatus.pending, 2673894238666))),
+        kNote(10, null, "test4", 1673894238666, Category.yellow, listOf(Tag.alpha, Tag.beta), listOf( kNotification(1, NotificationStatus.new, 2673894238666), kNotification(2, NotificationStatus.pending, 2673894238666))),
+        kNote(11, null, "test5", 1673894238666, Category.purple, listOf(Tag.alpha, Tag.beta), listOf( kNotification(1, NotificationStatus.new, 2673894238666), kNotification(2, NotificationStatus.pending, 2673894238666))),
+        kNote(12, null, "test6", 1673894238666, Category.purple, listOf(Tag.alpha, Tag.beta), listOf( kNotification(1, NotificationStatus.new, 2673894238666), kNotification(2, NotificationStatus.pending, 2673894238666))),
+
+        )
 }
